9665ddb691e322de78b94ff1fb7360ec8776ae42
AlekseiKapsh <alexseikapshienko4@gmail.com>
2025-01-23 18:44:27 +1000

    Добавьте файлы проекта.

---
+++ WebApplication12/Controllers/PageController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication12.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            ViewBag.Message = "Добро пожаловать на страницу!";
+            ViewBag.CurrentDate = DateTime.Now.ToString("F");
+            return View();
+        }
+
+        public IActionResult Greet(string name)
+        {
+            ViewBag.Message = $"Привет!";
+            return View();
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string message)
+        {
+            ViewBag.Message = message;
+            return View("DisplayMessage");
+        }
+    }
+}
